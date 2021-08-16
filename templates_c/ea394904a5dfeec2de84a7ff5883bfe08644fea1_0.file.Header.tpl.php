c55033ffc3f389327162325f15e9f5117212bc4e
Maria13579 <patymsr3@gmail.com>
2021-08-16 10:00:31 -0600

    B

---
+++ templates_c/ea394904a5dfeec2de84a7ff5883bfe08644fea1_0.file.Header.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-08-16 18:00:06
+  from 'C:\adobeTemp\htdocs\LaboratorioIII\templates\Cabeceras\Header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_611a8b86986223_95137543',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ea394904a5dfeec2de84a7ff5883bfe08644fea1' => 
+    array (
+      0 => 'C:\\adobeTemp\\htdocs\\LaboratorioIII\\templates\\Cabeceras\\Header.tpl',
+      1 => 1623690620,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_611a8b86986223_95137543 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+ <head>
+   <!--Import Google Icon Font-->
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">     
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+     <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+   
+  </head>
+ <body><?php }
+}
---
+++ templates_c/f7c01da6f11ab76010baa79d691a0d145855ded4_0.file.Footer.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-08-16 18:00:06
+  from 'C:\adobeTemp\htdocs\LaboratorioIII\templates\Cabeceras\Footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_611a8b869ce081_17106233',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f7c01da6f11ab76010baa79d691a0d145855ded4' => 
+    array (
+      0 => 'C:\\adobeTemp\\htdocs\\LaboratorioIII\\templates\\Cabeceras\\Footer.tpl',
+      1 => 1623424316,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_611a8b869ce081_17106233 (Smarty_Internal_Template $_smarty_tpl) {
+?>     <?php echo '<script'; ?>
+ type="text/javascript" src ="https://code.jquery.com/jquery-2.1.1.min.js"><?php echo '</script'; ?>
+>
+       <?php echo '<script'; ?>
+ src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"><?php echo '</script'; ?>
+>
+ </body>
+ </html><?php }
+}
