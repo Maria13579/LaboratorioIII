bf8b8049535d6bba11ac81d4069f9b697d81bd78
Maria13579 <patymsr3@gmail.com>
2021-08-17 23:47:08 -0600

    p

--- templates/Navs/BarraAdministrador.tpl
+++ templates/Navs/BarraAdministrador.tpl
@@ -1,6 +1,6 @@
 <div class="row">
 <div class="col s2">
-   <nav class="center-align teal lighten-2 ">OPCIONES</nav>
+   <nav class="center-align purple lighten-4">OPCIONES</nav>
       <div class="collection">
       <a href="?class=Direcciones&method=VisCrearUsuario" class="collection-item ">Crear Usuario</a>
       <a href="?class=Direcciones&method=VisIngresoProducto" class="collection-item">Agregar Producto</a>
--- templates_c/abb311972e556dde30471e78b0e161e9872cad8f_0.file.BarraAdministrador.tpl.php
+++ templates_c/abb311972e556dde30471e78b0e161e9872cad8f_0.file.BarraAdministrador.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-08-17 00:32:05
+/* Smarty version 3.1.39, created on 2021-08-18 07:44:28
   from 'C:\adobeTemp\htdocs\LaboratorioIII\templates\Navs\BarraAdministrador.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_611ae765546eb0_83403275',
+  'unifunc' => 'content_611c9e3cb7b2f5_86810103',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'abb311972e556dde30471e78b0e161e9872cad8f' => 
     array (
       0 => 'C:\\adobeTemp\\htdocs\\LaboratorioIII\\templates\\Navs\\BarraAdministrador.tpl',
-      1 => 1629153104,
+      1 => 1629265465,
       2 => 'file',
     ),
   ),
@@ -20,10 +20,10 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_611ae765546eb0_83403275 (Smarty_Internal_Template $_smarty_tpl) {
+function content_611c9e3cb7b2f5_86810103 (Smarty_Internal_Template $_smarty_tpl) {
 ?><div class="row">
 <div class="col s2">
-   <nav class="center-align teal lighten-2 ">OPCIONES</nav>
+   <nav class="center-align purple lighten-4">OPCIONES</nav>
       <div class="collection">
       <a href="?class=Direcciones&method=VisCrearUsuario" class="collection-item ">Crear Usuario</a>
       <a href="?class=Direcciones&method=VisIngresoProducto" class="collection-item">Agregar Producto</a>
